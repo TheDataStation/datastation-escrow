921577335beba3c4948db8b1454e21fafcc9e056
raulcf <raulcf@uchicago.edu>
2022-01-16 20:38:52 -0600

    sketching app registration

---
+++ dsapplicationregistration/example_registration.py
@@ -0,0 +1,14 @@
+from dsapplicationregistration.dsar_core import expose
+
+
+@expose
+def preprocess(a: str):
+    """preprocess all the data"""
+    return
+
+
+@expose
+def train(a: int):
+    """trains the model"""
+    return
+
