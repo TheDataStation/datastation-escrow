0e2897f5d95e98d8ff2bc4c89168117876468378
raulcf <raulcf@uchicago.edu>
2022-01-22 21:46:49 -0600

    added basic registration workflow

--- app_connectors/example_registration.py
+++ app_connectors/example_registration.py
@@ -1,20 +1,20 @@
-from dsapplicationregistration.dsar_core import expose
+from dsapplicationregistration import register
 
 
-@expose
+@register
 def preprocess(a: str):
     """preprocess all the data"""
     print("Hi, this is preprocess")
     return 33
 
 
-@expose(depends_on=[preprocess])
+@register(depends_on=[preprocess])
 def train(a: int):
     """trains the model"""
     return
 
 
-@expose(depends_on=[train])
+@register(depends_on=[train])
 def query_inference(q: int):
     """queries a model"""
     return
