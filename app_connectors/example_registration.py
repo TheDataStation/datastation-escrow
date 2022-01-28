29367c94e1ec377754b21b0f0346c34a9dfc49f0
KoS <kos@KoSdeMacBook-Pro.local>
2022-01-27 23:20:45 -0600

    gk setuptest done

--- app_connectors/example_registration.py
+++ app_connectors/example_registration.py
@@ -1,20 +1,23 @@
 from dsapplicationregistration import register
 
 
-@register
-def preprocess():
+@register()
+def preprocess(num_files: int):
     """preprocess all the data"""
     print("preprocess called")
+    print("Let's clean "+str(num_files)+" files!")
 
 
 @register(depends_on=[preprocess])
-def modeltrain():
+def modeltrain(num_models: int):
     """trains the model"""
     print("modeltrain called")
+    print("I want to train "+str(num_models)+" models!")
 
 
 @register(depends_on=[modeltrain])
-def predict():
+def predict(accuracy: int):
     """submits input to get predictions"""
     print("predict called")
+    print("Prediction accuracy is "+str(accuracy)+" percent :(")
 
