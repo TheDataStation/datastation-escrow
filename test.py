b8b3c3d6fdbd925e5de11226ef1c112f880c16c0
KoS <kos@KoSdeMacBook-Pro.local>
2022-01-29 21:53:23 -0600

    updating script

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import main
 import os
+import shutil
 from common import utils
 from models.user import *
+from models.policy import *
 
 ds_config = utils.parse_config("data_station_config.yaml")
 app_config = utils.parse_config("app_connector_config.yaml")
@@ -26,6 +28,40 @@ print(apis)
 print(api_dependencies)
 
 # Upload datasets
+
+# First clear the storage place
+
+folder = 'SM_storage'
+for filename in os.listdir(folder):
+    file_path = os.path.join(folder, filename)
+    if os.path.isfile(file_path) or os.path.islink(file_path):
+        os.unlink(file_path)
+    elif os.path.isdir(file_path):
+        shutil.rmtree(file_path)
+
+list_of_files = ["train-1.csv", "train-2.csv", "train-3.csv", "train-4.csv", "train-5.csv", "train-6.csv"]
+list_of_data_ids = []
+
+for cur_name in list_of_files:
+    cur_full_name = "test_file/" + cur_name
+    cur_file = open(cur_full_name, "rb")
+    cur_file_bytes = cur_file.read()
+    cur_res = client_api.upload_dataset(cur_name.split(".")[0], cur_file_bytes, cur_token)
+    list_of_data_ids.append(cur_res.data_id)
+    cur_file.close()
+
+# Upload Policies
+client_api.upload_policy(Policy(user_id=1, api="preprocess", data_id=list_of_data_ids[0]), cur_token)
+client_api.upload_policy(Policy(user_id=1, api="preprocess", data_id=list_of_data_ids[1]), cur_token)
+client_api.upload_policy(Policy(user_id=1, api="preprocess", data_id=list_of_data_ids[2]), cur_token)
+client_api.upload_policy(Policy(user_id=1, api="modeltrain", data_id=list_of_data_ids[3]), cur_token)
+client_api.upload_policy(Policy(user_id=1, api="modeltrain", data_id=list_of_data_ids[4]), cur_token)
+client_api.upload_policy(Policy(user_id=1, api="predict", data_id=list_of_data_ids[5]), cur_token)
+
+
+
+
+
 
 
 
