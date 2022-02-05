058396b1a649825084fc4c67ca5b9d79889befe6
youny626 <zhiru@uchicago.edu>
2022-02-04 20:20:16 -0600

    interceptor process now working

--- app_connectors/example_registration.py
+++ app_connectors/example_registration.py
@@ -1,14 +1,18 @@
+import os.path
 import pathlib
 
 from dsapplicationregistration import register
 import glob
+from common import utils
 
 @register()
 def preprocess():
     """preprocess all the data"""
     print("preprocess called")
     # files = glob.glob("/Users/zhiruzhu/Desktop/data_station/DataStation/SM_storage/**/*", recursive=True)
-    files = glob.glob("SM_storage_mount/**/**/**/*", recursive=True)
+    ds_config = utils.parse_config("data_station_config.yaml")
+    mount_path = pathlib.Path(ds_config["mount_path"]).absolute()
+    files = glob.glob(os.path.join(str(mount_path), "**/**/**/*"), recursive=True)
     # print(set(files))
     for file in set(files):
         # print(file)
@@ -16,6 +20,7 @@ def preprocess():
             with open(file, "r") as cur_file:
                 cur_file.readline()
             print("read ", file)
+
     return 0
 
 
