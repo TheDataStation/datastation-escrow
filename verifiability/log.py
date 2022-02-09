ad8a5d7aa5e5f3713dc8c1eb2b84499a28c0ec10
KoS <kos@KoSdeMacBook-Pro.local>
2022-02-09 15:15:45 -0600

    quick changes

--- verifiability/log.py
+++ verifiability/log.py
@@ -8,11 +8,11 @@ class Log:
     Definition of log entries. Each has a log_entry_type that determines the meaning of this entry. log_entry_types are
     declared in an Enum type.
     """
-    IntentDefiniteDES = namedtuple('IntentFiniteDES', 'log_entry_type, agent_id, api_id, list_DES')
-    IntentIndefiniteDES = namedtuple('IntentIndefiniteDES', 'log_entry_type, agent_id, api_id')
-    IntentPolicyMatch = namedtuple('IntentPolicyMatch', 'log_entry_type, agent_id, api_id, list_DES')
-    IntentPolicyMismatch = namedtuple('IntentPolicyMismatch', 'log_entry_type, agent_id, api_id, list_DES')
-    GrantFExecution = namedtuple('GrantFExecution', 'log_entry_type, agent_id, api_id')
+    IntentDefiniteDES = namedtuple('IntentFiniteDES', 'log_entry_type, agent_id, api, list_DES')
+    IntentIndefiniteDES = namedtuple('IntentIndefiniteDES', 'log_entry_type, agent_id, api')
+    IntentPolicyMatch = namedtuple('IntentPolicyMatch', 'log_entry_type, agent_id, api, list_DES')
+    IntentPolicyMismatch = namedtuple('IntentPolicyMismatch', 'log_entry_type, agent_id, api, list_DES')
+    GrantFExecution = namedtuple('GrantFExecution', 'log_entry_type, agent_id, api')
 
     """
     An internal enum class to declare the log entry types
@@ -40,37 +40,37 @@ class Log:
             # TODO: config
             pass
 
-    def log_intent_definite(self, agent_id: int, api_id: int, list_des: [int]):
+    def log_intent_definite(self, agent_id: int, api: str, list_des: [int]):
         entry = self.IntentDefiniteDES(log_entry_type=self.LogEntryType.INTENT_DEFINITE_DES,
                                        agent_id=agent_id,
-                                       api_id=api_id,
+                                       api=api,
                                        list_DES=list_des)
         self._log(entry)
 
-    def log_intent_indefinite(self, agent_id: int, api_id: int):
+    def log_intent_indefinite(self, agent_id: int, api: str):
         entry = self.IntentIndefiniteDES(log_entry_type=self.LogEntryType.INTENT_INDEFINITE_DES,
                                          agent_id=agent_id,
-                                         api_id=api_id)
+                                         api=api)
         self._log(entry)
 
-    def log_intent_policy_match(self, agent_id: int, api_id: int, list_des: [int]):
+    def log_intent_policy_match(self, agent_id: int, api: str, list_des: [int]):
         entry = self.IntentPolicyMatch(log_entry_type=self.LogEntryType.INTENT_POLICY_MATCH,
                                        agent_id=agent_id,
-                                       api_id=api_id,
+                                       api=api,
                                        list_DES=list_des)
         self._log(entry)
 
-    def log_intent_policy_mismatch(self, agent_id: int, api_id: int, list_des: [int]):
+    def log_intent_policy_mismatch(self, agent_id: int, api: str, list_des: [int]):
         entry = self.IntentPolicyMismatch(log_entry_type=self.LogEntryType.INTENT_POLICY_MISMATCH,
                                           agent_id=agent_id,
-                                          api_id=api_id,
+                                          api=api,
                                           list_DES=list_des)
         self._log(entry)
 
-    def log_grant_f_execution(self, agent_id, api_id):
+    def log_grant_f_execution(self, agent_id, api):
         entry = self.GrantFExecution(log_entry_type=self.LogEntryType.GRANT_F_EXECUTION,
                                      agent_id=agent_id,
-                                     api_id=api_id)
+                                     api=api)
         self._log(entry)
 
     def _log(self, entry):
