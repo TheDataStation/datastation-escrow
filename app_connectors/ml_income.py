317aae7202d4a66b693ab0a7856d35fa5da4627e
KoS <kos@KoSdeMacBook-Pro.local>
2022-02-26 20:30:53 -0600

    b4 merge main

--- app_connectors/ml_income.py
+++ app_connectors/ml_income.py
@@ -10,30 +10,30 @@ from common import utils
 
 # This version here is for full_trust. We may or may not need this.
 
-@register()
-def train_income_model():
-    """train a logistic regression model on income data"""
-    print("starting train model")
-    ds_path = str(pathlib.Path(os.path.dirname(os.path.abspath(__file__))).parent)
-    ds_config = utils.parse_config(os.path.join(ds_path, "data_station_config.yaml"))
-    mount_path = pathlib.Path(ds_config["mount_path"]).absolute()
-    files = glob.glob(os.path.join(str(mount_path), "**/**/**/*"), recursive=True)
-    Xtrains = []
-    ytrains = []
-    for file in set(files):
-        if pathlib.Path(file).is_file():
-            array_to_append = file.split('/')[-1].split('.')[0][-1]
-            cur_np_ele = np.load(file)
-            if array_to_append == "X":
-                Xtrains.append(cur_np_ele)
-            else:
-                ytrains.append(cur_np_ele)
-    Xtrains = np.concatenate(Xtrains)
-    ytrains = np.concatenate(ytrains)
-    income_model = LogisticRegression()
-    income_model.fit(Xtrains, ytrains)
-    print(income_model)
-    return income_model
+# @register()
+# def train_income_model():
+#     """train a logistic regression model on income data"""
+#     print("starting train model")
+#     ds_path = str(pathlib.Path(os.path.dirname(os.path.abspath(__file__))).parent)
+#     ds_config = utils.parse_config(os.path.join(ds_path, "data_station_config.yaml"))
+#     mount_path = pathlib.Path(ds_config["mount_path"]).absolute()
+#     files = glob.glob(os.path.join(str(mount_path), "**/**/**/*"), recursive=True)
+#     Xtrains = []
+#     ytrains = []
+#     for file in set(files):
+#         if pathlib.Path(file).is_file():
+#             array_to_append = file.split('/')[-1].split('.')[0][-1]
+#             cur_np_ele = np.load(file)
+#             if array_to_append == "X":
+#                 Xtrains.append(cur_np_ele)
+#             else:
+#                 ytrains.append(cur_np_ele)
+#     Xtrains = np.concatenate(Xtrains)
+#     ytrains = np.concatenate(ytrains)
+#     income_model = LogisticRegression()
+#     income_model.fit(Xtrains, ytrains)
+#     print(income_model)
+#     return income_model
 
 # This version here is for no_trust.
 
@@ -75,3 +75,30 @@ def train_income_model():
 #     # income_model = LogisticRegression()
 #     # income_model.fit(Xtrains, ytrains)
 #     # return income_model
+
+@register()
+def train_income_model():
+    """train a logistic regression model on income data"""
+    print("starting train model")
+    ds_path = str(pathlib.Path(os.path.dirname(os.path.abspath(__file__))).parent)
+    ds_config = utils.parse_config(os.path.join(ds_path, "data_station_config.yaml"))
+    mount_path = pathlib.Path(ds_config["mount_path"]).absolute()
+    files = glob.glob(os.path.join(str(mount_path), "**/**/**/*"), recursive=True)
+    Xtrains = []
+    ytrains = []
+    for file in set(files):
+        if pathlib.Path(file).is_file():
+            print(file)
+            cur_np_ele = np.load(file)
+            # array_to_append = file.split('/')[-1].split('.')[0][-1]
+            # cur_np_ele = np.load(file)
+            # if array_to_append == "X":
+            #     Xtrains.append(cur_np_ele)
+            # else:
+            #     ytrains.append(cur_np_ele)
+    # Xtrains = np.concatenate(Xtrains)
+    # ytrains = np.concatenate(ytrains)
+    # income_model = LogisticRegression()
+    # income_model.fit(Xtrains, ytrains)
+    # print(income_model)
+    # return income_model
