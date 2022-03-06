7a3ae0b78b89f3af1bf014597b9fc2b5bc5332d0
KoS <kos@wirelessprv-10-194-93-54.near.illinois.edu>
2022-03-05 18:43:53 -0600

    start ddprofiler

--- app_connectors/aurum_test.py
+++ app_connectors/aurum_test.py
@@ -1,8 +1,6 @@
-import os
-
 from dsapplicationregistration import register
 
+
 @register()
-def start_es():
-    print("Starting elastic search server")
-    # os.system("/Users/kos/Desktop/AurumTest/elasticsearch-6.0.0/bin/elasticsearch")
+def hello():
+    return 0
