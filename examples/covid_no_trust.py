57992167f1375efc4c51a03f7bb038bf01ecadb5
KoS <kos@KoSdeMacBook-Pro.local>
2022-05-13 13:50:24 -0500

    covid: get accessible fnames

--- examples/covid_no_trust.py
+++ examples/covid_no_trust.py
@@ -10,3 +10,14 @@ from common import utils
 @register()
 def train_covid_model():
     print("starting covid model")
+    ds_path = str(pathlib.Path(os.path.dirname(os.path.abspath(__file__))).parent)
+    ds_config = utils.parse_config(os.path.join(ds_path, "data_station_config.yaml"))
+    mount_path = pathlib.Path(ds_config["mount_path"]).absolute()
+    files = glob.glob(os.path.join(str(mount_path), "**/**/**/*"), recursive=True)
+    f_name_in_order = []
+    for file in set(files):
+        if pathlib.Path(file).is_file():
+            f_name_in_order.append(file)
+    f_name_in_order.sort(key=lambda x: int(x.split('/')[-2]))
+    print(f_name_in_order)
+
