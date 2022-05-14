16954872cf6ef32137de1d8a58e7604b4ec7f81d
KoS <kos@KoSdeMacBook-Pro.local>
2022-05-13 20:57:48 -0500

    mapping done; img recover success

--- examples/covid_no_trust.py
+++ examples/covid_no_trust.py
@@ -5,6 +5,9 @@ import pandas as pd
 import pickle
 import time
 from sklearn.model_selection import train_test_split
+from PIL import Image
+import io
+
 
 from dsapplicationregistration import register
 import glob
@@ -16,13 +19,16 @@ def train_covid_model():
     ds_path = str(pathlib.Path(os.path.dirname(os.path.abspath(__file__))).parent)
     ds_config = utils.parse_config(os.path.join(ds_path, "data_station_config.yaml"))
     mount_path = pathlib.Path(ds_config["mount_path"]).absolute()
-    # TODO: let's build an in-memory dictionary that holds filename-id mappings
-    # files = glob.glob(os.path.join(str(mount_path), "**/**/**/*"), recursive=True)
-    # f_name_in_order = []
-    # for file in set(files):
-    #     if pathlib.Path(file).is_file():
-    #         f_name_in_order.append(file)
-    # f_name_in_order.sort(key=lambda x: int(x.split('/')[-2]))
+    files = glob.glob(os.path.join(str(mount_path), "**/**/**/*"), recursive=True)
+    f_name_id_dict = {}
+    for file in set(files):
+        if pathlib.Path(file).is_file():
+            f_name_short = file.split('/')[-1]
+            f_id = file.split('/')[-2]
+            f_name_id_dict[f_name_short] = f_id
+
+    # # Checking that the dictionary is correct
+    # print(f_name_id_dict)
 
     # Start the ML code
 
@@ -44,8 +50,14 @@ def train_covid_model():
     valid_label = list()
 
     for _, row in train_df.iterrows():
-        cur_file_path = os.path.join(str(mount_path), row["filename"])
-        print(cur_file_path)
+        cur_file_path = os.path.join(str(mount_path), f_name_id_dict.get(row["filename"]), row["filename"])
+        f = open(cur_file_path, "rb")
+        cur_file_bytes = f.read()
+        # These bytes should be decrpted already, we convert it to object
+        image_np_arr = pickle.loads(cur_file_bytes)
+        f.close()
+        image = Image.fromarray(image_np_arr)
+        image.show()
         if row["class"] == "positive":
             train_label.append(1)
         else:
