3af695d37d42843d7eaf98002bed9b3c76d17167
KoS <kos@KoSdeMacBook-Pro.local>
2022-05-14 19:22:18 -0500

    before fitting model

--- examples/covid_no_trust.py
+++ examples/covid_no_trust.py
@@ -2,11 +2,13 @@ import os
 import pathlib
 import numpy as np
 import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
 import pickle
 import time
 from sklearn.model_selection import train_test_split
-from PIL import Image
-import io
+from keras.preprocessing.image import ImageDataGenerator
+from csv import writer
 
 
 from dsapplicationregistration import register
@@ -87,11 +89,66 @@ def train_covid_model():
         else:
             valid_label.append(0)
 
-    # print(np.shape(train_data))
-    # print(np.shape(valid_data))
+    train_data = np.asarray(train_data).reshape(len(train_data), 200, 200, 3)
+    valid_data = np.asarray(valid_data).reshape(len(valid_data), 200, 200, 3)
+
+    # print(len(train_data))
+    # print(len(valid_data))
     # print(train_label)
     # print(valid_label)
 
-    # Let's first finish the code here.
+    # Start the actual model training process
+    train_datagen = ImageDataGenerator(rescale=1. / 255.,
+                                       rotation_range=40,
+                                       width_shift_range=0.2,
+                                       height_shift_range=0.2,
+                                       shear_range=0.2,
+                                       zoom_range=0.2,
+                                       horizontal_flip=True,
+                                       vertical_flip=True)
+    test_datagen = ImageDataGenerator(rescale=1.0 / 255.)
+
+    train_gen = train_datagen.flow(train_data, train_label, batch_size=64)
+    valid_gen = test_datagen.flow(valid_data, valid_label, batch_size=64)
+
+    base_model = tf.keras.applications.ResNet50V2(weights='imagenet',
+                                                  input_shape=(200, 200, 3),
+                                                  include_top=False)
+
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    model = tf.keras.Sequential([
+        base_model,
+        tf.keras.layers.GlobalAveragePooling2D(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    class EvaluateEpochEnd(tf.keras.callbacks.Callback):
+        def __init__(self, cur_test_data):
+            self.test_data = cur_test_data
+
+        def on_epoch_end(self, epoch, logs={}):
+            global prev_epoch_time
+            scores = self.model.evaluate(self.test_data, verbose=0)
+            print('\nTesting loss: {}, accuracy: {}\n'.format(scores[0], scores[1]))
+            cur_epoch_time = time.time()
+            with open("ml_acc.csv", 'a+') as f:
+                writer_object = writer(f)
+                writer_object.writerow([scores[0], scores[1], cur_epoch_time - prev_epoch_time])
+            prev_epoch_time = cur_epoch_time
+
+    callbacks = [
+        tf.keras.callbacks.ModelCheckpoint("covid_classifier_model.h5", save_best_only=True, verbose=0),
+        tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=2, verbose=1),
+        # EvaluateEpochEnd(test_gen),
+    ]
+
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001),
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
 
     print("data station overhead is", time.time() - start_time)
