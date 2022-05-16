d6eb1706201283ed1a684124198aab4641595a3b
KoS <kos@KoSdeMacBook-Pro.local>
2022-05-15 20:54:49 -0500

    covidml: no touching ml_file_no_trust

--- integration_tests/covid_no_trust.py
+++ integration_tests/covid_no_trust.py
@@ -68,13 +68,13 @@ if __name__ == '__main__':
 
     # First clear ml_file_no_trust/training
 
-    no_trust_folder = 'integration_tests/ml_file_no_trust/training_covid'
-    for filename in os.listdir(no_trust_folder):
-        file_path = os.path.join(no_trust_folder, filename)
-        if os.path.isfile(file_path) or os.path.islink(file_path):
-            os.unlink(file_path)
-        elif os.path.isdir(file_path):
-            shutil.rmtree(file_path)
+    # no_trust_folder = 'integration_tests/ml_file_no_trust/training_covid'
+    # for filename in os.listdir(no_trust_folder):
+    #     file_path = os.path.join(no_trust_folder, filename)
+    #     if os.path.isfile(file_path) or os.path.islink(file_path):
+    #         os.unlink(file_path)
+    #     elif os.path.isdir(file_path):
+    #         shutil.rmtree(file_path)
 
     # Clear the storage place
 
