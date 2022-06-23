9ed47c24b7fc5f24304cb9d1beb49ac908138069
KoS <kos@KoSdeMacBook-Pro.local>
2022-06-22 21:34:57 -0500

    sql full b4 connector

---
+++ examples/sql_full.py
@@ -0,0 +1,5 @@
+from dsapplicationregistration import register
+
+@register()
+def run_sql_query():
+    print("test SQL queries")
--- integration_tests/sql_full.py
+++ integration_tests/sql_full.py
@@ -3,8 +3,11 @@
 import os
 import pathlib
 import main
+import shutil
 
 from common.general_utils import parse_config
+from common.pydantic_models.user import User
+from common.pydantic_models.policy import Policy
 
 if __name__ == '__main__':
 
@@ -25,6 +28,41 @@ if __name__ == '__main__':
     log_path = client_api.log.log_path
     if os.path.exists(log_path):
         os.remove(log_path)
+
+    # We upload one data owner: owner of the DB company.db, with two relations INFO and PAYMENT
+    # We also upload a data user
+    num_users = 2
+
+    for cur_num in range(num_users):
+        cur_uname = "user" + str(cur_num)
+        client_api.create_user(User(user_name=cur_uname, password="string"))
+
+    # Clear the storage place
+
+    folder = 'SM_storage'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+
+    # Now we upload company.db to DS
+    cur_token = client_api.login_user("user1", "string")["access_token"]
+
+    DE_to_upload = "integration_tests/sql_test/company.db"
+    cur_file = open(DE_to_upload, "rb")
+    cur_file_bytes = cur_file.read()
+    cur_optimistic_flag = False
+    name_to_upload = "company.db"
+    client_api.upload_dataset(name_to_upload,
+                              cur_file_bytes,
+                              "file",
+                              cur_optimistic_flag,
+                              cur_token, )
+
+    # Upload policy saying user0 can access company.db
+    client_api.upload_policy(Policy(user_id=1, api="run_sql_query", data_id=1), cur_token)
 
     # Shut down
     client_api.shut_down(ds_config)
