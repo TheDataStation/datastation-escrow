d8e85a949ffc3085385aa89c131b8508c872965d
youny626 <zhiru@uchicago.edu>
2022-06-29 22:50:30 -0500

    private query working version with global context

--- common/utils.py
+++ common/utils.py
@@ -1,6 +1,23 @@
+import glob
 import yaml
+import pathlib
+import os
 
 def parse_config(path_to_config):
     with open(path_to_config) as config_file:
         res_config = yaml.load(config_file, Loader=yaml.FullLoader)
     return res_config
+
+def get_all_files():
+    ds_path = str(pathlib.Path(os.path.dirname(os.path.abspath(__file__))).parent)
+    ds_config = parse_config(os.path.join(ds_path, "data_station_config.yaml"))
+    mount_path = pathlib.Path(ds_config["mount_path"]).absolute()
+    files = set(glob.glob(os.path.join(str(mount_path), "**/**/**/*"), recursive=True))
+
+    results = []
+    for file in files:
+        # print(file)
+        if pathlib.Path(file).is_file():
+            results.append(file)
+
+    return results
