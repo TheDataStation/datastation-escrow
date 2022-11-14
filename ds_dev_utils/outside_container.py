048c487362deb8f07feb26bf7e2c4d805f229059
Christopher Zhu <chz@uchicago.edu>
2022-11-14 13:57:26 -0600

    lesgo, mounted directory and created docker image

--- ds_dev_utils/outside_container.py
+++ ds_dev_utils/outside_container.py
@@ -1,7 +1,27 @@
 import docker
+import time
 
 client = docker.from_env()
 
+# create image from dockerfile
 image, log = client.images.build(path="./docker/images", tag="ds_docker")
-print(log)
-print(client.containers.run(image, "python setup.py"))
+print(image,log)
+
+# run a container with command. It's detached
+container = client.containers.run(image, 
+                                  "python setup.py",
+                                  detach=True,
+                                  tty=True,
+                                  volumes={'/Users/christopherzhu/Documents/chidata/DataStation/ds_dev_utils/my_data': {'bind':'/mnt/data', 'mode':'rw'}},
+                                  remove=True,
+                                  )
+
+time.sleep(3)
+
+# Docker prints logs as bytes
+print(container.logs().decode("utf-8"))
+
+running_containers = client.containers.list()
+
+# container.stop()
+print(running_containers)
