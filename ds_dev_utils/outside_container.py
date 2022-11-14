7b7bb7723a1c9e97ee35e3bfece2e242b996f1d2
Christopher Zhu <chz@uchicago.edu>
2022-11-14 16:34:29 -0600

    make container not shut down the moment setup.py is run

--- ds_dev_utils/outside_container.py
+++ ds_dev_utils/outside_container.py
@@ -9,17 +9,20 @@ print(image,log)
 
 # run a container with command. It's detached
 container = client.containers.run(image, 
-                                  "python setup.py",
                                   detach=True,
                                   tty=True,
                                   volumes={'/Users/christopherzhu/Documents/chidata/DataStation/ds_dev_utils/my_data': {'bind':'/mnt/data', 'mode':'rw'}},
                                   remove=True,
                                   )
 
+code, ret = container.exec_run("python setup.py")
+
 time.sleep(3)
 
+
 # Docker prints logs as bytes
-print(container.logs().decode("utf-8"))
+print(ret.decode("utf-8"))
+# print(container.logs().decode("utf-8"))
 
 running_containers = client.containers.list()
 
