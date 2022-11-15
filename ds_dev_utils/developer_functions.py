1a2fcec6f9b508da09ead4db9a06182ec5f7eb50
Christopher Zhu <chz@uchicago.edu>
2022-11-15 15:12:26 -0600

    make into a class

--- ds_dev_utils/developer_functions.py
+++ ds_dev_utils/developer_functions.py
@@ -32,78 +32,73 @@ def docker_cp(container, src, dst):
 
     # return docker.put_archive(src, dst)
 
+class ds_docker:
+    def __init__(self,function_file, connector_file, data_dir, image):
+        """
+        Initializes a docker container with mount point data_dir, with
+        image given. Loads function file into container.
+        Image currently does nothing.
 
-def stop_and_prune(client, container):
-    """
-    Stops the given Docker container instance and removes any non-running containers
+        Parameters:
+        function_file: the file containing the functions to be run
+        connector_file: the file containing the connectors that call
+        functions within the function_file
+        data_dir: directory to mount data to container
+        image: docker image (from hub) to build off on
 
-    Parameters:
-     client: the docker client to prune
-     container: container to stop
+        Returns:
+        """
+        client = docker.from_env()
 
-    Returns:
-     Nothing
-    """
-    container.stop()
-    client.containers.prune()
+        # create image from dockerfile
+        self.image, log = client.images.build(path="./docker/images", tag="ds_docker")
+        print(self.image, log)
 
+        # run a container with command. It's detached so it runs in the background.
+        self.container = client.containers.run(self.image,
+                                        detach=True,
+                                        tty=True,
+                                        volumes={data_dir: {
+                                            'bind': '/mnt/data', 'mode': 'rw'}},
+                                        remove=True,
+                                        )
 
-def ds_docker_init(function_file, connector_file, data_dir, image):
-    """
-    Initializes a docker container with mount point data_dir, with
-     image given. Loads function file into container.
-    Image currently does nothing.
+        # copy the function file into the container
+        docker_cp(self.container,
+                function_file,
+                "/usr/src/ds/functions")
 
-    Parameters:
-     function_file: the file containing the functions to be run
-     connector_file: the file containing the connectors that call
-      functions within the function_file
-     data_dir: directory to mount data to container
-     image: docker image (from hub) to build off on
+        # run setup script
+        code, ret = self.container.exec_run("python setup.py")
+        time.sleep(4)
 
-    Returns:
-    """
-    client = docker.from_env()
+        print(os.getcwd())
 
-    # create image from dockerfile
-    image, log = client.images.build(path="./docker/images", tag="ds_docker")
-    print(image, log)
+        # Docker prints logs as bytes
+        print(ret.decode("utf-8"))
+        # print(container.logs().decode("utf-8"))
 
-    # run a container with command. It's detached so it runs in the background.
-    container = client.containers.run(image,
-                                      detach=True,
-                                      tty=True,
-                                      volumes={data_dir: {
-                                          'bind': '/mnt/data', 'mode': 'rw'}},
-                                      remove=True,
-                                      )
+    def stop_and_prune(self):
+        """
+        Stops the given Docker container instance and removes any non-running containers
 
-    # copy the function file into the container
-    docker_cp(container,
-              function_file,
-              "/usr/src/ds/functions")
+        Parameters:
+        client: the docker client to prune
+        container: container to stop
 
-    # run setup script
-    code, ret = container.exec_run("python setup.py")
-    time.sleep(4)
+        Returns:
+        Nothing
+        """
+        self.container.stop()
+        self.client.containers.prune()
 
-    print(os.getcwd())
-
-    # Docker prints logs as bytes
-    print(ret.decode("utf-8"))
-    # print(container.logs().decode("utf-8"))
-
-    stop_and_prune(client, container)
-
-def ds_docker_run(connector, args, kwargs):
-    """
-    
-    """
-    return
-
+    def run():
+        """
+        """
+        return
 
 if __name__ == "__main__":
-    ds_docker_init(
+    session = ds_docker(
         '/Users/christopherzhu/Documents/chidata/DataStation/ds_dev_utils/functions/example_one.py',
         "connector_file",
         '/Users/christopherzhu/Documents/chidata/DataStation/ds_dev_utils/my_data',
