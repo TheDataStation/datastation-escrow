f755494ecfe8f349c53f698677b121bbd778b79f
Christopher Zhu <chz@uchicago.edu>
2022-11-14 23:37:15 -0600

    put stuff in functions and clean up code

--- ds_dev_utils/docker/images/setup.py
+++ ds_dev_utils/docker/images/setup.py
@@ -5,6 +5,9 @@ print("hi")
 f = open("/mnt/data/hi.txt", "r")
 print(f.read())
 
+f = open("/usr/src/ds/functions/example_one.py", "r")
+print(f.read())
+
 for i in range(5):
     print("hi")
     time.sleep(1)
--- ds_dev_utils/outside_container.py
+++ ds_dev_utils/outside_container.py
@@ -1,5 +1,7 @@
 import docker
 import time
+import os
+import tarfile
 
 
 def docker_cp(container, src, dst):
@@ -15,36 +17,87 @@ def docker_cp(container, src, dst):
     Returns:
      The error (if any) of the function put_archive
     """
-    
+
+    os.chdir(os.path.dirname(src))
+    srcname = os.path.basename(src)
+
+    tar = tarfile.open(src + '.tar', mode='w')
+    try:
+        tar.add(srcname)
+    finally:
+        tar.close()
+
+    data = open(src + '.tar', 'rb').read()
+    container.put_archive(dst, data)
 
     # return docker.put_archive(src, dst)
 
-client = docker.from_env()
+def stop_and_prune(client, container):
+    """
+    Stops the given Docker container instance and removes any non-running containers
 
-# create image from dockerfile
-image, log = client.images.build(path="./docker/images", tag="ds_docker")
-print(image,log)
+    Parameters:
+     client: the docker client to prune
+     container: container to stop
+    
+    Returns:
+     Nothing
+    """
+    container.stop()
+    client.containers.prune()
 
-# run a container with command. It's detached
-container = client.containers.run(image, 
-                                  detach=True,
-                                  tty=True,
-                                  volumes=
-                                  {'/Users/christopherzhu/Documents/chidata/DataStation/ds_dev_utils/my_data':
-                                   {'bind':'/mnt/data', 'mode':'rw'}},
-                                  remove=True,
-                                  )
+def ds_docker_init(function_file, connector_file, data_dir, image):
+    """
+    Initializes a docker container with mount point data_dir, with
+     image given. Loads function file into container.
+    Image currently does nothing.
 
-code, ret = container.exec_run("python setup.py")
+    Parameters:
+     function_file: the file containing the functions to be run
+     connector_file: the file containing the connectors that call
+      functions within the function_file
+     data_dir: directory to mount data to container
+     image: docker image (from hub) to build off on
 
-time.sleep(3)
+    Returns:
+    """
+    client = docker.from_env()
 
+    # create image from dockerfile
+    image, log = client.images.build(path="./docker/images", tag="ds_docker")
+    print(image,log)
 
-# Docker prints logs as bytes
-print(ret.decode("utf-8"))
-# print(container.logs().decode("utf-8"))
+    # run a container with command. It's detached so it runs in the background.
+    container = client.containers.run(image, 
+                                    detach=True,
+                                    tty=True,
+                                    volumes=
+                                    {data_dir:
+                                    {'bind':'/mnt/data', 'mode':'rw'}},
+                                    remove=True,
+                                    )
 
-running_containers = client.containers.list()
+    # copy the function file into the container
+    docker_cp(container,
+            function_file,
+            "/usr/src/ds/functions")
 
-# container.stop()
-print(running_containers)
+    # run setup script
+    code, ret = container.exec_run("python setup.py")
+    time.sleep(3)
+
+    print(os.getcwd())
+
+    # Docker prints logs as bytes
+    print(ret.decode("utf-8"))
+    # print(container.logs().decode("utf-8"))
+
+    stop_and_prune(client, container)
+
+if __name__ == "__main__":
+    ds_docker_init(
+        '/Users/christopherzhu/Documents/chidata/DataStation/ds_dev_utils/functions/example_one.py',
+        "connector_file",
+        '/Users/christopherzhu/Documents/chidata/DataStation/ds_dev_utils/my_data',
+        "image"
+    )
