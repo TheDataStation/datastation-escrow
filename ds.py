a038cbf11f286cd3abe0ab4657d277348bcdfb1c
Christopher Zhu <chz@uchicago.edu>
2022-12-01 17:13:10 -0600

    moving client_api functions into ds.py

--- ds.py
+++ ds.py
@@ -1,19 +1,34 @@
 import os
 import sys
-from dbservice.database_api import set_checkpoint_table_paths, recover_db_from_snapshots
-from storagemanager.storage_manager import StorageManager
-from stagingstorage.staging_storage import StagingStorage
-from verifiability.log import Log
-from writeaheadlog.write_ahead_log import WAL
-from crypto.key_manager import KeyManager
-from gatekeeper.gatekeeper import Gatekeeper
-from clientapi.client_api import ClientAPI
-from common.general_utils import parse_config
-from dbservice import database_api
-from Interceptor import interceptor
 import multiprocessing
 import pathlib
 import time
+import pickle
+
+from common.pydantic_models.api import API
+from common.pydantic_models.user import User
+from common.pydantic_models.response import Response
+from common.pydantic_models.policy import Policy
+
+from common.general_utils import parse_config
+
+from dbservice.database_api import set_checkpoint_table_paths, recover_db_from_snapshots
+from storagemanager.storage_manager import StorageManager
+from stagingstorage.staging_storage import StagingStorage
+from policybroker import policy_broker
+from dataregister import data_register
+from verifiability.log import Log
+from writeaheadlog.write_ahead_log import WAL
+from crypto import cryptoutils as cu
+from crypto.key_manager import KeyManager
+from gatekeeper.gatekeeper import Gatekeeper
+from clientapi.client_api import ClientAPI
+from dbservice import database_api
+from dbservice.database import engine
+from dbservice.database_api import clear_checkpoint_table_paths
+from Interceptor import interceptor
+from dsapplicationregistration.dsar_core import clear_register
+from userregister import user_register
 
 class DSConfig:
     def __init__(self, ds_config):
@@ -44,6 +59,14 @@ class DSConfig:
 
 class DataStation:
     def __init__(self, ds_config, app_config, need_to_recover=False):
+        """
+        The general class that creates and initializes a Data Station.
+
+        Parameters:
+         ds_config: dictionary of general config options
+         app_config: dictionary of app connector options TODO: store this as variables
+         need_to_recover: bool that specifies if needs to recover from previous database
+        """
         # parse config file
         self.config = DSConfig(ds_config)
 
@@ -98,7 +121,7 @@ class DataStation:
         # set up an instance of the key manager
         self.key_manager = KeyManager()
 
-        # set up the application registration in the gatekeeper
+        # set up the the gatekeeper
         self.connector_name = app_config["connector_name"]
         self.connector_module_path = app_config["connector_module_path"]
         self.gatekeeper = Gatekeeper(
@@ -153,6 +176,255 @@ class DataStation:
             self.cur_staging_data_id = 1
 
 
+    def create_user(self, user: User, user_sym_key=None, user_public_key=None):
+
+        # First we decide which user_id to use from ClientAPI.cur_user_id field
+        user_id = self.cur_user_id
+        self.cur_user_id += 1
+
+        # Part one: register this user's symmetric key and public key
+        if self.trust_mode == "no_trust":
+            self.key_manager.store_agent_symmetric_key(user_id, user_sym_key)
+            self.key_manager.store_agent_public_key(user_id, user_public_key)
+
+        # Part two: Call the user_register to register the user in the DB
+        if self.trust_mode == "full_trust":
+            response = user_register.create_user(user_id,
+                                                 user.user_name,
+                                                 user.password,)
+        else:
+            response = user_register.create_user(user_id,
+                                                 user.user_name,
+                                                 user.password,
+                                                 self.write_ahead_log,
+                                                 self.key_manager,)
+
+        if response.status == 1:
+            return Response(status=response.status, message=response.message)
+
+        return Response(status=response.status, message=response.message)
+
+
+    @staticmethod
+    def get_all_apis():
+        # Call policy_broker directly
+        return policy_broker.get_all_apis()
+
+
+    @staticmethod
+    def get_all_api_dependencies():
+        # Call policy_broker directly
+        return policy_broker.get_all_dependencies()
+
+
+    def upload_dataset(self,
+                       username,
+                       data_name,
+                       data_in_bytes,
+                       data_type,
+                       optimistic,
+                       token,
+                       original_data_size=None):
+
+        # Decide which data_id to use from ClientAPI.cur_data_id field
+        data_id = self.cur_data_id
+        self.cur_data_id += 1
+
+        # We first call SM to store the data
+        # Note that SM needs to return access_type (how can the data element be accessed)
+        # so that data_register can register this info
+
+        storage_manager_response = self.storage_manager.store(data_name,
+                                                              data_id,
+                                                              data_in_bytes,
+                                                              data_type,)
+        if storage_manager_response.status == 1:
+            return storage_manager_response
+
+        # Storing data is successful. We now call data_register to register this data element in DB
+        # Note: for file, access_type is the fullpath to the file
+        access_type = storage_manager_response.access_type
+
+        if self.trust_mode == "full_trust":
+            data_register_response = data_register.register_data_in_DB(data_id,
+                                                                       data_name,
+                                                                       username,
+                                                                       data_type,
+                                                                       access_type,
+                                                                       optimistic)
+        else:
+            data_register_response = data_register.register_data_in_DB(data_id,
+                                                                       data_name,
+                                                                       username,
+                                                                       data_type,
+                                                                       access_type,
+                                                                       optimistic,
+                                                                       self.write_ahead_log,
+                                                                       self.key_manager,
+                                                                       original_data_size)
+        if data_register_response.status != 0:
+            return Response(status=data_register_response.status,
+                            message=data_register_response.message)
+
+        return data_register_response
+
+
+    def remove_dataset(self, username, data_name):
+
+        # First we call data_register to remove the existing dataset from the database
+        if self.trust_mode == "full_trust":
+            data_register_response = data_register.remove_data(data_name,
+                                                               username,)
+        else:
+            data_register_response = data_register.remove_data(data_name,
+                                                               username,
+                                                               self.write_ahead_log,
+                                                               self.key_manager,)
+        if data_register_response.status != 0:
+            return Response(status=data_register_response.status, message=data_register_response.message)
+
+        # At this step we have removed the record about the dataset from DB
+        # Now we remove its actual content from SM
+        storage_manager_response = self.storage_manager.remove(data_name,
+                                                               data_register_response.data_id,
+                                                               data_register_response.type,)
+
+        # If SM removal failed
+        if storage_manager_response.status == 1:
+            return storage_manager_response
+
+        return Response(status=data_register_response.status, message=data_register_response.message)
+
+
+    def upload_policy(self, username, policy: Policy):
+        if self.trust_mode == "full_trust":
+            response = policy_broker.upload_policy(policy,
+                                                   username,)
+        else:
+            response = policy_broker.upload_policy(policy,
+                                                   username,
+                                                   self.write_ahead_log,
+                                                   self.key_manager,)
+
+        return Response(status=response.status, message=response.message)
+
+
+    def bulk_upload_policies(self, username, policies):
+        # This code here is unpolished. Just for testing purposes.
+        if self.trust_mode == "full_trust":
+            response = database_api.bulk_upload_policies(policies)
+            return response
+        else:
+            response = policy_broker.bulk_upload_policies(policies,
+                                                          username,
+                                                          self.write_ahead_log,
+                                                          self.key_manager,)
+            return response
+
+
+    def remove_policy(self, username, policy: Policy):
+        if self.trust_mode == "full_trust":
+            response = policy_broker.remove_policy(policy,
+                                                   username,)
+        else:
+            response = policy_broker.remove_policy(policy,
+                                                   username,
+                                                   self.write_ahead_log,
+                                                   self.key_manager,)
+
+        return Response(status=response.status, message=response.message)
+
+
+    @staticmethod
+    def get_all_policies():
+        return policy_broker.get_all_policies()
+
+
+    def call_api(self, username, api: API, exec_mode, *args, **kwargs):
+        # get caller's UID
+        cur_user = database_api.get_user_by_user_name(User(user_name=username, ))
+        # If the user doesn't exist, something is wrong
+        if cur_user.status == -1:
+            print("Something wrong with the current user")
+            return Response(status=1, message="Something wrong with the current user")
+        cur_user_id = cur_user.data[0].id
+
+        res = self.gatekeeper.call_api(api,
+                                  cur_user_id,
+                                  exec_mode,
+                                  *args,
+                                  **kwargs)
+        # Only when the returned status is 0 can we release the result
+        if res.status == 0:
+            api_result = res.result
+            # We still need to encrypt the results using the caller's symmetric key if in no_trust_mode.
+            if self.trust_mode == "no_trust":
+                caller_symmetric_key = self.key_manager.get_agent_symmetric_key(cur_user_id)
+                api_result = cu.encrypt_data_with_symmetric_key(cu.to_bytes(api_result), caller_symmetric_key)
+            return api_result
+        # In this case we need to put result into staging storage, so that they can be released later
+        elif res.status == -1:
+            api_result = res.result[0]
+            data_ids_accessed = res.result[1]
+            # We first convert api_result to bytes because we need to store it in staging storage
+            # In full_trust mode, we convert it to bytes directly
+            if self.trust_mode == "full_trust":
+                api_result = cu.to_bytes(api_result)
+            # In no_trust mode, we encrypt it using caller's symmetric key
+            else:
+                caller_symmetric_key = self.key_manager.get_agent_symmetric_key(cur_user_id)
+                api_result = cu.encrypt_data_with_symmetric_key(cu.to_bytes(api_result), caller_symmetric_key)
+
+            # print(api_result)
+            # print(data_ids_accessed)
+
+            # Call staging storage to store the bytes
+
+            # Decide which data_id to use from ClientAPI.cur_data_id field
+            staging_data_id = self.cur_staging_data_id
+            self.cur_staging_data_id += 1
+
+            staging_storage_response = self.staging_storage.store(staging_data_id,
+                                                                  api_result)
+            if staging_storage_response.status == 1:
+                return staging_storage_response
+
+            # Storing into staging storage is successful. We now call data_register to register this staging DE in DB.
+            # We need to store to both the staged table and the provenance table.
+
+            # Full_trust mode
+            if self.trust_mode == "full_trust":
+                # Staged table
+                data_register_response_staged = data_register.register_staged_in_DB(staging_data_id,
+                                                                                    cur_user_id,
+                                                                                    api,)
+                # Provenance table
+                data_register_response_provenance = data_register.register_provenance_in_DB(staging_data_id,
+                                                                                            data_ids_accessed,)
+            else:
+                # Staged table
+                data_register_response_staged = data_register.register_staged_in_DB(staging_data_id,
+                                                                                    cur_user_id,
+                                                                                    api,
+                                                                                    self.write_ahead_log,
+                                                                                    self.key_manager,
+                                                                                    )
+                # Provenance table
+                data_register_response_provenance = data_register.register_provenance_in_DB(staging_data_id,
+                                                                                            data_ids_accessed,
+                                                                                            cur_user_id,
+                                                                                            self.write_ahead_log,
+                                                                                            self.key_manager,
+                                                                                            )
+            if data_register_response_staged.status != 0 or data_register_response_provenance.status != 0:
+                return Response(status=data_register_response_staged.status,
+                                message="internal database error")
+            res_msg = "Staged data ID " + str(staging_data_id)
+            return res_msg
+        else:
+            return res.message
+
+
     def recover_db_from_wal(self):
         # Step 1: restruct the DB
         self.write_ahead_log.recover_db_from_wal(self.key_manager)
@@ -186,3 +458,38 @@ class DataStation:
         with open("symmetric_keys.pkl", "rb") as keys:
             agents_symmetric_key = pickle.load(keys)
             self.key_manager.agents_symmetric_key = agents_symmetric_key
+
+    def shut_down(self):
+        # print("shut down...")
+        mount_point = self.config.mount_point
+        unmount_status = os.system("umount " + str(mount_point))
+        counter = 0
+        while unmount_status != 0:
+            time.sleep(1)
+            unmount_status = os.system("umount " + str(mount_point))
+            if counter == 10:
+                print("Unmount failed")
+                exit(1)
+
+        assert os.path.ismount(mount_point) is False
+        self.interceptor_process.join()
+
+        # Clear DB, app register, and db.checkpoint
+        engine.dispose()
+        clear_register()
+        clear_checkpoint_table_paths()
+
+        print("shut down complete")
+
+
+if __name__ == "__main__":
+    print("Main")
+    # First parse test_config files and command line arguments
+
+    # https://docs.python.org/3/library/argparse.html
+    # (potentiall need to re-write some key-values from clg)
+    data_station_config = parse_config(sys.argv[1])
+    app_connector_config = parse_config(sys.argv[2])
+    ds = DataStation(data_station_config, app_connector_config)
+
+    # run_system(parse_config("data_station_config.yaml"))
