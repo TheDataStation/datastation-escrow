9372e4def25bd8c83732b7a60b570c46ceeea914
Christopher Zhu <chz@uchicago.edu>
2023-01-19 16:19:29 -0600

    remove annoying print in interceptor reads

--- Interceptor/interceptor.py
+++ Interceptor/interceptor.py
@@ -321,7 +321,7 @@ class Xmp(Fuse):
 
 
             def read(self, length, offset):
-                print("Interceptor: I am reading " + str(self.file_path))
+                # print("Interceptor: I am reading " + str(self.file_path))
 
                 if self.iolock:
                     self.iolock.acquire()
