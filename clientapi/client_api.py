ea7aa2191b51ab3252bf6044f84270482e3024ca
Christopher Zhu <chz@uchicago.edu>
2023-02-08 01:29:19 -0600

    client api workinge i think

--- clientapi/client_api.py
+++ clientapi/client_api.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import time
+import sys
 from flask import Flask, request
 from multiprocessing import Process, Event, Queue, Manager
 
@@ -24,26 +25,35 @@ from writeaheadlog.write_ahead_log import WAL
 from crypto.key_manager import KeyManager
 from crypto import cryptoutils as cu
 from dbservice.database import engine
-from dsapplicationregistration.dsar_core import clear_register
 from dbservice.database_api import clear_checkpoint_table_paths
 from ds import DataStation
 
 app = Flask(__name__)
 
-def flask_thread(port, config):
-    ds = DataStation(config, None)
+def flask_thread(port, config, app_config):
+    ds = DataStation(config, app_config)
 
     # TODO: make ds stateless
 
     # create user
-    @app.route("/call_api")
+    @app.route("/call_api", methods=['post'])
     def call_api():
         unpickled = (request.get_data())
-        print(unpickled)
-        user_dict = pickle.loads(unpickled)
-        print("User dictionary is", user_dict)
+        # print(unpickled)
+        args = pickle.loads(unpickled)
+        # print("Call API Arguments are:", args)
 
-        return ds.create_user(user_dict['user'], user_dict['user_sym_key'], user_dict['user_pub_key'])
+        return pickle.dumps(ds.call_api(args['username'], args['api'], args['exec_mode'], ds, *args['args'], **args['kwargs']))
+
+    # create user
+    @app.route("/create_user", methods=['post'])
+    def create_user():
+        unpickled = (request.get_data())
+        # print(unpickled)
+        args = pickle.loads(unpickled)
+        print("Received 'Create User'. User Arguments are:", args, file=sys.stdout)
+
+        return pickle.dumps(ds.create_user(args['user'], args.get('user_sym_key', None), args.get('user_public_key', None)))
 
     # log in
     @app.route("/login_user")
@@ -75,11 +85,11 @@ class ClientAPI:
         return username
 
     def __init__(self,
-                 config, port=8080):
+                 config, app_config, port=8080):
 
         self.port = port
 
-        self.server = Process(target=flask_thread, args=(self.port,config))
+        self.server = Process(target=flask_thread, args=(self.port,config, app_config))
         return
 
 
@@ -92,348 +102,6 @@ class ClientAPI:
 
 
         print("client_api shut down complete")
-
-
-    # list application apis
-
-    @staticmethod
-    def get_all_apis(token):
-
-        # Perform authentication
-        user_register.authenticate_user(token)
-
-        # Call policy_broker directly
-        return policy_broker.get_all_apis()
-
-    # list application api dependencies
-
-    @staticmethod
-    def get_all_api_dependencies(token):
-
-        # Perform authentication
-        user_register.authenticate_user(token)
-
-        # Call policy_broker directly
-        return policy_broker.get_all_dependencies()
-
-    # upload data element
-
-    def upload_dataset(self,
-                       data_name,
-                       data_in_bytes,
-                       data_type,
-                       optimistic,
-                       token,
-                       original_data_size=None):
-
-        self.validate_and_get_username(token)
-        # parse HTTP request data (payload with the arguments)
-        # self.ds.upload_dataset(parsed_data_name, parsed_data_in_bytes ...etc.)
-
-        # Perform authentication
-        cur_username = user_register.authenticate_user(token)
-
-        # Decide which data_id to use from ClientAPI.cur_data_id field
-        data_id = self.cur_data_id
-        self.cur_data_id += 1
-
-        # We first call SM to store the data
-        # Note that SM needs to return access_type (how can the data element be accessed)
-        # so that data_register can register this info
-
-        storage_manager_response = self.storage_manager.store(data_name,
-                                                              data_id,
-                                                              data_in_bytes,
-                                                              data_type,)
-        if storage_manager_response.status == 1:
-            return storage_manager_response
-
-        # Storing data is successful. We now call data_register to register this data element in DB
-        # Note: for file, access_type is the fullpath to the file
-        access_type = storage_manager_response.access_type
-
-        if self.trust_mode == "full_trust":
-            data_register_response = data_register.register_data_in_DB(data_id,
-                                                                       data_name,
-                                                                       cur_username,
-                                                                       data_type,
-                                                                       access_type,
-                                                                       optimistic)
-        else:
-            data_register_response = data_register.register_data_in_DB(data_id,
-                                                                       data_name,
-                                                                       cur_username,
-                                                                       data_type,
-                                                                       access_type,
-                                                                       optimistic,
-                                                                       self.write_ahead_log,
-                                                                       self.key_manager,
-                                                                       original_data_size)
-        if data_register_response.status != 0:
-            return Response(status=data_register_response.status,
-                            message=data_register_response.message)
-
-        return data_register_response
-
-    # remote data element
-
-    def remove_dataset(self, data_name, token):
-
-        # Perform authentication
-        cur_username = user_register.authenticate_user(token)
-
-        # First we call data_register to remove the existing dataset from the database
-        if self.trust_mode == "full_trust":
-            data_register_response = data_register.remove_data(data_name,
-                                                               cur_username,)
-        else:
-            data_register_response = data_register.remove_data(data_name,
-                                                               cur_username,
-                                                               self.write_ahead_log,
-                                                               self.key_manager,)
-        if data_register_response.status != 0:
-            return Response(status=data_register_response.status, message=data_register_response.message)
-
-        # At this step we have removed the record about the dataset from DB
-        # Now we remove its actual content from SM
-        storage_manager_response = self.storage_manager.remove(data_name,
-                                                               data_register_response.data_id,
-                                                               data_register_response.type,)
-
-        # If SM removal failed
-        if storage_manager_response.status == 1:
-            return storage_manager_response
-
-        return Response(status=data_register_response.status, message=data_register_response.message)
-
-    # create_policies
-
-    def upload_policy(self, policy: Policy, token):
-
-        # Perform authentication
-        cur_username = user_register.authenticate_user(token)
-
-        if self.trust_mode == "full_trust":
-            response = policy_broker.upload_policy(policy,
-                                                   cur_username,)
-        else:
-            response = policy_broker.upload_policy(policy,
-                                                   cur_username,
-                                                   self.write_ahead_log,
-                                                   self.key_manager,)
-
-        return Response(status=response.status, message=response.message)
-
-    # bulk upload policies
-    # This is for testing purposes
-
-    def bulk_upload_policies(self, policies, token):
-
-        # Perform authentication
-        cur_username = user_register.authenticate_user(token)
-
-        # This code here is unpolished. Just for testing purposes.
-        if self.trust_mode == "full_trust":
-            response = database_api.bulk_upload_policies(policies)
-            return response
-        else:
-            response = policy_broker.bulk_upload_policies(policies,
-                                                          cur_username,
-                                                          self.write_ahead_log,
-                                                          self.key_manager,)
-            return response
-
-    # delete_policies
-
-    def remove_policy(self, policy: Policy, token):
-
-        # Perform authentication
-        cur_username = user_register.authenticate_user(token)
-
-        if self.trust_mode == "full_trust":
-            response = policy_broker.remove_policy(policy,
-                                                   cur_username,)
-        else:
-            response = policy_broker.remove_policy(policy,
-                                                   cur_username,
-                                                   self.write_ahead_log,
-                                                   self.key_manager,)
-
-        return Response(status=response.status, message=response.message)
-
-    # list all available policies
-
-    @staticmethod
-    def get_all_policies():
-        return policy_broker.get_all_policies()
-
-    # data users actually calling the application apis
-
-    def call_api(self, api: API, token, exec_mode, *args, **kwargs):
-
-        # Perform authentication
-        cur_username = user_register.authenticate_user(token)
-        # get caller's UID
-        cur_user = database_api.get_user_by_user_name(User(user_name=cur_username, ))
-        # If the user doesn't exist, something is wrong
-        if cur_user.status == -1:
-            print("Something wrong with the current user")
-            return Response(status=1, message="Something wrong with the current user")
-        cur_user_id = cur_user.data[0].id
-
-        res = gatekeeper.call_api(api,
-                                  cur_user_id,
-                                  exec_mode,
-                                  self.log,
-                                  self.key_manager,
-                                  self.accessible_data_dict,
-                                  self.data_accessed_dict,
-                                  *args,
-                                  **kwargs)
-        # Only when the returned status is 0 can we release the result
-        if res.status == 0:
-            api_result = res.result
-            # We still need to encrypt the results using the caller's symmetric key if in no_trust_mode.
-            if self.trust_mode == "no_trust":
-                caller_symmetric_key = self.key_manager.get_agent_symmetric_key(cur_user_id)
-                api_result = cu.encrypt_data_with_symmetric_key(cu.to_bytes(api_result), caller_symmetric_key)
-            return api_result
-        # In this case we need to put result into staging storage, so that they can be released later
-        elif res.status == -1:
-            api_result = res.result[0]
-            data_ids_accessed = res.result[1]
-            # We first convert api_result to bytes because we need to store it in staging storage
-            # In full_trust mode, we convert it to bytes directly
-            if self.trust_mode == "full_trust":
-                api_result = cu.to_bytes(api_result)
-            # In no_trust mode, we encrypt it using caller's symmetric key
-            else:
-                caller_symmetric_key = self.key_manager.get_agent_symmetric_key(cur_user_id)
-                api_result = cu.encrypt_data_with_symmetric_key(cu.to_bytes(api_result), caller_symmetric_key)
-
-            # print(api_result)
-            # print(data_ids_accessed)
-
-            # Call staging storage to store the bytes
-
-            # Decide which data_id to use from ClientAPI.cur_data_id field
-            staging_data_id = self.cur_staging_data_id
-            self.cur_staging_data_id += 1
-
-            staging_storage_response = self.staging_storage.store(staging_data_id,
-                                                                  api_result)
-            if staging_storage_response.status == 1:
-                return staging_storage_response
-
-            # Storing into staging storage is successful. We now call data_register to register this staging DE in DB.
-            # We need to store to both the staged table and the provenance table.
-
-            # Full_trust mode
-            if self.trust_mode == "full_trust":
-                # Staged table
-                data_register_response_staged = data_register.register_staged_in_DB(staging_data_id,
-                                                                                    cur_user_id,
-                                                                                    api,)
-                # Provenance table
-                data_register_response_provenance = data_register.register_provenance_in_DB(staging_data_id,
-                                                                                            data_ids_accessed,)
-            else:
-                # Staged table
-                data_register_response_staged = data_register.register_staged_in_DB(staging_data_id,
-                                                                                    cur_user_id,
-                                                                                    api,
-                                                                                    self.write_ahead_log,
-                                                                                    self.key_manager,
-                                                                                    )
-                # Provenance table
-                data_register_response_provenance = data_register.register_provenance_in_DB(staging_data_id,
-                                                                                            data_ids_accessed,
-                                                                                            cur_user_id,
-                                                                                            self.write_ahead_log,
-                                                                                            self.key_manager,
-                                                                                            )
-            if data_register_response_staged.status != 0 or data_register_response_provenance.status != 0:
-                return Response(status=data_register_response_staged.status,
-                                message="internal database error")
-            res_msg = "Staged data ID " + str(staging_data_id)
-            return res_msg
-        else:
-            return res.message
-
-    # data users gives a staged DE ID and tries to release it
-    def release_staged_DE(self, staged_ID, token):
-
-        # Perform authentication
-        cur_username = user_register.authenticate_user(token)
-        # get caller's UID
-        cur_user = database_api.get_user_by_user_name(User(user_name=cur_username, ))
-        # If the user doesn't exist, something is wrong
-        if cur_user.status == -1:
-            print("Something wrong with the current user")
-            return Response(status=1, message="Something wrong with the current user")
-        cur_user_id = cur_user.data[0].id
-
-        # First get the API call that generated this staged DE
-        api = database_api.get_api_for_staged_id(staged_ID)
-
-        # Then get the currently accessible DEs for the <cur_user_id, api> combo
-        accessible_data_ids = policy_broker.get_user_api_info(cur_user_id, api)
-
-        # Then get the parent_ids for this staged DE (all accessed DEs used to create this DE)
-        accessed_data_ids = database_api.get_parent_id_for_staged_id(staged_ID)
-
-        # Then check if accessed_data_ids is a subset of accessible_data_ids
-        # If yes, we can release the staged DE
-        if set(accessed_data_ids).issubset(set(accessible_data_ids)):
-            res = cu.from_bytes(self.staging_storage.release(staged_ID))
-            return res
-
-    # print out the contents of the log
-
-    def read_full_log(self):
-        self.log.read_full_log(self.key_manager)
-
-    # print out the contents of the WAL
-
-    def read_wal(self):
-        self.write_ahead_log.read_wal(self.key_manager)
-
-    # recover DB from the contents of the WAL
-
-    def recover_db_from_wal(self):
-        # Step 1: restruct the DB
-        self.write_ahead_log.recover_db_from_wal(self.key_manager)
-
-        # Step 2: reset self.cur_user_id from DB
-        user_id_resp = database_api.get_user_with_max_id()
-        if user_id_resp.status == 1:
-            self.cur_user_id = user_id_resp.data[0].id + 1
-        else:
-            self.cur_user_id = 1
-        print("User ID to use after recovering DB is: "+str(self.cur_user_id))
-
-        # Step 3: reset self.cur_data_id from DB
-        data_id_resp = database_api.get_data_with_max_id()
-        if data_id_resp.status == 1:
-            self.cur_data_id = data_id_resp.data[0].id + 1
-        else:
-            self.cur_data_id = 1
-        print("Data ID to use after recovering DB is: " + str(self.cur_data_id))
-
-    # For testing purposes: persist keys to a file
-
-    def save_symmetric_keys(self):
-        with open("symmetric_keys.pkl", 'ab') as keys:
-            agents_symmetric_key = pickle.dumps(self.key_manager.agents_symmetric_key)
-            keys.write(agents_symmetric_key)
-
-    # For testing purposes: read keys from a file
-
-    def load_symmetric_keys(self):
-        with open("symmetric_keys.pkl", "rb") as keys:
-            agents_symmetric_key = pickle.load(keys)
-            self.key_manager.agents_symmetric_key = agents_symmetric_key
-
 
 if __name__ == "__main__":
     print("Client API")
--- integration_new/example_one.py
+++ integration_new/example_one.py
@@ -1,11 +1,30 @@
 import os
 import shutil
 import pathlib
+import requests
+import pickle
+import time
 
 from common import general_utils
 from common.pydantic_models.user import User
 from common.pydantic_models.policy import Policy
-from ds import DataStation
+from clientapi.client_api import ClientAPI
+
+
+def call_api(username, api, exec_mode, *args, **kwargs):
+    api_dict = {'username': username,
+                'api': api,
+                'exec_mode': exec_mode,
+                'args': args,
+                'kwargs': kwargs}
+
+    api_response = requests.post("http://localhost:8080/call_api",
+                                 data=pickle.dumps(api_dict),
+                                 # headers={'Content-Type': 'application/octet-stream'},
+                                 )
+
+    return pickle.loads(api_response.content)
+
 
 if __name__ == '__main__':
 
@@ -29,43 +48,87 @@ if __name__ == '__main__':
     ds_storage_path = str(pathlib.Path(ds_config["storage_path"]).absolute())
     mount_point = str(pathlib.Path(ds_config["mount_path"]).absolute())
 
-    ds = DataStation(ds_config, app_config)
+    ds = ClientAPI(ds_config, app_config)
 
-    log_path = ds.data_station_log.log_path
-    if os.path.exists(log_path):
-        os.remove(log_path)
+    # log_path = ds.data_station_log.log_path
+    # if os.path.exists(log_path):
+    #     os.remove(log_path)
+
+    ds.start_server()
+
+    # just to wait for server to start
+    time.sleep(3)
+
+    # START REMOTE USER LOGIN AND ACTIONS
 
     # Step 1: We create two new users of the Data Station: jerry and david
-    ds.create_user(User(user_name="jerry", password="string"))
-    ds.create_user(User(user_name="david", password="123456"))
+    u1 = {'user': User(user_name="jerry", password="string")}
+    create_user_response = requests.post("http://localhost:8080/create_user",
+                                         data=pickle.dumps(u1),
+                                         # headers={'Content-Type': 'application/octet-stream'},
+                                         )
+    print(pickle.loads(create_user_response.content))
+    u2 = {'user': User(user_name="david", password="123456")}
+    create_user_response = requests.post("http://localhost:8080/create_user",
+                                         data=pickle.dumps(u2),
+                                         # headers={'Content-Type': 'application/octet-stream'},
+                                         )
+    print(pickle.loads(create_user_response.content))
 
     # Step 2: Jerry logs in and uploads three datasets
     # He uploads DE1 and DE3 in sealed mode, and uploads DE2 in enclave mode.
     for cur_num in range(3):
         cur_file_index = (cur_num % 6) + 1
-        cur_full_name = "integration_tests/test_file_full_trust/train-" + str(cur_file_index) + ".csv"
+        cur_full_name = "integration_tests/test_file_full_trust/train-" + \
+            str(cur_file_index) + ".csv"
         cur_file = open(cur_full_name, "rb")
         cur_file_bytes = cur_file.read()
         cur_optimistic_flag = False
         if cur_num == 1:
             cur_optimistic_flag = True
         name_to_upload = "file-" + str(cur_num + 1)
-        cur_res = ds.call_api("jerry",
-                              "upload_dataset",
-                              "pessimistic",
-                              ds,
-                              "jerry",
-                              name_to_upload,
-                              cur_file_bytes,
-                              "file",
-                              cur_optimistic_flag,)
+        ret = call_api('jerry',
+                       'upload_dataset',
+                       'pessimistic',
+                       "jerry",
+                       name_to_upload,
+                       cur_file_bytes,
+                       "file",
+                       cur_optimistic_flag)
+
+        # cur_res = ds.call_api("jerry",
+        #                       "upload_dataset",
+        #                       "pessimistic",
+        #                       ds,
+        #                       "jerry",
+        #                       name_to_upload,
+        #                       cur_file_bytes,
+        #                       "file",
+        #                       cur_optimistic_flag,)
 
     # Step 3: jerry creates a policy saying that david can discover how many lines his files have
     # for DE [1, 3].
     policy_one = Policy(user_id=2, api="line_count", data_id=1)
-    ds.call_api("jerry", "upload_policy", "pessimistic", ds, "jerry", policy_one)
+    api_dict = {'username': 'jerry',
+                'api': 'upload_policy',
+                'exec_mode': 'pessimistic',
+                'args': ["jerry", policy_one],
+                'kwargs': {}}
+    api_response = requests.post("http://localhost:8080/call_api",
+                                 data=pickle.dumps(api_dict),
+                                 # headers={'Content-Type': 'application/octet-stream'},
+                                 )
+
     policy_two = Policy(user_id=2, api="line_count", data_id=3)
-    ds.call_api("jerry", "upload_policy", "pessimistic", ds, "jerry", policy_two)
+    api_dict = {'username': 'jerry',
+                'api': 'upload_policy',
+                'exec_mode': 'pessimistic',
+                'args': ["jerry", policy_two],
+                'kwargs': {}}
+    api_response = requests.post("http://localhost:8080/call_api",
+                                 data=pickle.dumps(api_dict),
+                                 # headers={'Content-Type': 'application/octet-stream'},
+                                 )
 
     # Step 4: david calls the API line_count. He runs it in optimistic mode.
     line_count_res = ds.call_api("david", "line_count", "optimistic")
@@ -73,5 +136,3 @@ if __name__ == '__main__':
 
     # Last step: shut down the Data Station
     ds.shut_down()
-
-
