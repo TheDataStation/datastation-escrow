0a243ac29606cba416a3e8b67ea0100f8b22d90e
Christopher Zhu <chz@uchicago.edu>
2023-02-23 16:52:38 -0600

    add returns

--- escrowapi/escrow_api.py
+++ escrowapi/escrow_api.py
@@ -15,7 +15,7 @@ class EscrowAPI:
 
     @classmethod
     def get_all_accessible_des(cls):
-        cls.__comp.do_stuff()
+        return cls.__comp.get_all_accessible_des()
 
     @classmethod
     def register_dataset(cls,
@@ -26,17 +26,17 @@ class EscrowAPI:
                         optimistic,
                         original_data_size=None,
                         ):
-        cls.__comp.register_dataset(username, data_name, data_in_bytes, data_type, optimistic, original_data_size)
+        return cls.__comp.register_dataset(username, data_name, data_in_bytes, data_type, optimistic, original_data_size)
 
     @classmethod
     def upload_policy(cls, username, user_id, api, data_id):
-        cls.__comp.upload_policy(username, user_id, api, data_id)
+        return cls.__comp.upload_policy(username, user_id, api, data_id)
 
 
     @classmethod
     def suggest_share(cls, username, agents, functions, data_elements):
-        cls.__comp.suggest_share(username, agents, functions, data_elements)
+        return cls.__comp.suggest_share(username, agents, functions, data_elements)
 
     @classmethod
     def ack_data_in_share(cls, username, share_id, data_id):
-        cls.__comp.ack_data_in_share(username, share_id, data_id)
+        return cls.__comp.ack_data_in_share(username, share_id, data_id)
