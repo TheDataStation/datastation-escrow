cb0193a3934acb3cea0056b221ea5cb601a6d173
Christopher Zhu <chz@uchicago.edu>
2023-02-23 16:45:44 -0600

    new file

---
+++ ds_dev_utils/docker/images/escrow_api_docker.py
@@ -0,0 +1,12 @@
+import requests
+
+class EscrowAPIDocker:
+    def __init__():
+        return
+    def get_accessible_des():
+        # some implementation of requests to communicate with flask docker server
+        ret = requests.post("")
+        return ret
+
+    def get_de():
+        requests.post("")
