66ef3d650260dc5e826d7d32f1a947ec3ca44d26
KoS <kos@KoSdeMBP.businesscenter>
2023-02-24 21:11:07 -0600

    get_accessible_de in progress

--- integration_new/different_de.py
+++ integration_new/different_de.py
@@ -77,17 +77,16 @@ if __name__ == '__main__':
                                de_access_param_2,
                                optimistic_flag_2, )
 
-    # # Step 3: jerry suggests a share saying david can discovery how many lines his files have
-    # # for DE [1, 3].
-    # agents = [2]
-    # functions = ["line_count"]
-    # data_elements = [1, 3]
-    # ds.call_api("jerry", "suggest_share", None, None, "jerry", agents, functions, data_elements)
-    #
-    # # Step 4: jerry acknowledges this share
-    # for data_id in data_elements:
-    #     ds.call_api("jerry", "ack_data_in_share", None, None, "jerry", data_id, 1)
-    #
+    # Step 3: jerry suggests a share saying david can access these two des.
+    agents = [2]
+    functions = ["get_data"]
+    data_elements = [1, 2]
+    ds.call_api("jerry", "suggest_share", None, None, "jerry", agents, functions, data_elements)
+
+    # Step 4: jerry acknowledges this share
+    for data_id in data_elements:
+        ds.call_api("jerry", "ack_data_in_share", None, None, "jerry", data_id, 1)
+
     # # Step 5: david calls the API line_count. He runs it in optimistic mode.
     # line_count_res = ds.call_api("david", "line_count", 1, "pessimistic")
     # print("The result of line count is:", line_count_res)
