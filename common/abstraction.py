3a322c2e0d8f201ae69130678c9f355d0c40ecef
KoS <kos@KoSdeMacBook-Pro.local>
2023-02-28 16:49:49 -0600

    adding NYC opendata DE

--- common/abstraction.py
+++ common/abstraction.py
@@ -1,6 +1,7 @@
 import os
 import json
 import psycopg2
+import requests
 
 
 class DataElement:
@@ -21,7 +22,7 @@ class DataElement:
         self.type = type
         self.access_param = access_param
         self.enc_key = enc_key
-        if self.type == "postgres":
+        if self.type != "file":
             self.access_param = json.loads(self.access_param)
 
     def get_data(self):
@@ -44,20 +45,34 @@ class DataElement:
             for row in cursor:
                 res.append(row)
             return res
+        elif self.type == "opendata":
+            api_key = self.access_param["api_key"]
+            endpoint = self.access_param["endpoint"]
+            headers = {'X-App-Token': api_key}
+            result = requests.get(endpoint, headers=headers)
+            return result.json()[0:100]
         else:
             return 0
 
 
 if __name__ == "__main__":
-    de_one = DataElement(1, "file", "a_cool_file")
+    de_one = DataElement(1, "a_cool_file", "file", "a_cool_file")
     data_one = de_one.get_data()
-    de_access_param_obj = {"credentials": "host='psql-mock-database-cloud.postgres.database.azure.com' "
-                                          "dbname='booking1677197406169yivvxvruhvmbvqzh' "
-                                          "user='gyhksqubzvuzcrdbhovifciu@psql-mock-database-cloud' "
-                                          "password='mbaqbkpppcmhsdbqukeutann'",
-                           "query": "SELECT * FROM bookings"}
-    de_access_param_2 = json.dumps(de_access_param_obj)
-    de_two = DataElement(2, "postgres", de_access_param_2)
+    de_access_param_obj_2 = {"credentials": "host='psql-mock-database-cloud.postgres.database.azure.com' "
+                                            "dbname='booking1677197406169yivvxvruhvmbvqzh' "
+                                            "user='gyhksqubzvuzcrdbhovifciu@psql-mock-database-cloud' "
+                                            "password='mbaqbkpppcmhsdbqukeutann'",
+                             "query": "SELECT * FROM bookings"}
+    de_access_param_2 = json.dumps(de_access_param_obj_2)
+    de_two = DataElement(2, "postgres-table", "postgres", de_access_param_2)
     data_two = de_two.get_data()
+    with open("integration_new/test_files/credentials/nyc.txt", 'r') as file:
+        api_key = file.read()
+    de_access_param_obj_3 = {"api_key": api_key,
+                             "endpoint": "https://data.cityofnewyork.us/resource/ic3t-wcy2.json"}
+    de_access_param_3 = json.dumps(de_access_param_obj_3)
+    de_three = DataElement(3, "job_application", "opendata", de_access_param_3)
+    data_three = de_three.get_data()
     print(data_one)
     print(data_two)
+    print(data_three)
