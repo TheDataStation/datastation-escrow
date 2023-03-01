7ec5961083b1a02a503b173bc93ad6755d0d3224
KoS <kos@KoSdeMacBook-Pro.local>
2023-03-01 16:22:28 -0600

    clean example_one_client

--- integration_new/example_one_client.py
+++ integration_new/example_one_client.py
@@ -1,13 +1,7 @@
-import os
-import shutil
-import pathlib
 import requests
 import pickle
-import time
 
-from common import general_utils
 from common.pydantic_models.user import User
-from common.pydantic_models.policy import Policy
 
 
 def call_api(username, api, exec_mode, *args, **kwargs):
@@ -32,22 +26,6 @@ def call_api(username, api, exec_mode, *args, **kwargs):
 
 if __name__ == '__main__':
 
-    # if os.path.exists("data_station.db"):
-    #     os.remove("data_station.db")
-
-    # folders = ['SM_storage', 'Staging_storage']
-    # for folder in folders:
-    #     for filename in os.listdir(folder):
-    #         file_path = os.path.join(folder, filename)
-    #         if os.path.isfile(file_path) or os.path.islink(file_path):
-    #             os.unlink(file_path)
-    #         elif os.path.isdir(file_path):
-    #             shutil.rmtree(file_path)
-
-    # log_path = "ds_log.pkl"
-    # if os.path.exists(log_path):
-    #     os.remove(log_path)
-
     # START REMOTE USER LOGIN AND ACTIONS
 
     # Step 1: We create two new users of the Data Station: jerry and david
@@ -68,33 +46,29 @@ if __name__ == '__main__':
     # He uploads DE1 and DE3 in sealed mode, and uploads DE2 in enclave mode.
     for cur_num in range(3):
         cur_file_index = (cur_num % 6) + 1
-        cur_full_name = "integration_new/test_files/plaintext/train-" + \
-            str(cur_file_index) + ".csv"
+        cur_full_name = "integration_new/test_files/plaintext/train-" + str(cur_file_index) + ".csv"
         cur_file = open(cur_full_name, "rb")
         cur_file_bytes = cur_file.read()
         cur_optimistic_flag = False
         if cur_num == 1:
             cur_optimistic_flag = True
         name_to_upload = "file-" + str(cur_num + 1)
-        ret = call_api('jerry',
-                       'register_data',
-                       None,
-                       None,
-                       "jerry",
-                       name_to_upload,
-                       cur_file_bytes,
-                       "file",
-                       cur_optimistic_flag)
-
-        # cur_res = ds.call_api("jerry",
-        #                       "upload_dataset",
-        #                       "pessimistic",
-        #                       ds,
-        #                       "jerry",
-        #                       name_to_upload,
-        #                       cur_file_bytes,
-        #                       "file",
-        #                       cur_optimistic_flag,)
+        register_res = call_api("jerry",
+                                "register_data",
+                                None,
+                                None,
+                                "jerry",
+                                name_to_upload,
+                                "file",
+                                name_to_upload,
+                                cur_optimistic_flag, )
+        upload_res = call_api("jerry",
+                              "upload_data",
+                              None,
+                              None,
+                              "jerry",
+                              register_res.data_id,
+                              cur_file_bytes, )
 
     # Step 3: jerry creates a policy saying that david can discover how many lines his files have
     # for DE [1, 3].
@@ -108,5 +82,5 @@ if __name__ == '__main__':
         call_api("jerry", "ack_data_in_share", None, None, "jerry", data_id, 1)
 
     # Step 5: david calls the API line_count. He runs it in optimistic mode.
-    line_count_res = call_api("david", "line_count", 1, "optimistic")
+    line_count_res = call_api("david", "line_count", 1, "pessimistic")
     print("The result of line count is:", line_count_res)
