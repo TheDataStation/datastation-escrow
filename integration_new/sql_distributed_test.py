d2447d47f53b2a606b0ac7696000065d699ecdfa
KoS <kos@KoSdeMBP.businesscenter>
2023-04-05 22:07:39 -0500

    zero trust tpch in progress

--- integration_new/sql_distributed_test.py
+++ integration_new/sql_distributed_test.py
@@ -8,11 +8,11 @@ from main import initialize_system
 def data_gen(num_partitions):
     """
     This function generates tpch data for testing different silos.
-    Output goes to integration_new/test_files/sql.
+    Output goes to integration_new/test_files/sql_plain.
     """
     # Define input/output directories
     input_dir = "/Users/kos/Desktop/TPCH-data/tpch-1MB/"
-    output_dir = "integration_new/test_files/sql/"
+    output_dir = "integration_new/test_files/sql_plain/"
     # Create a dictionary to store the schemas
     tpch_schemas = {"customer": ["c_custkey", "c_name", "c_address", "c_nationkey", "c_phone", "c_acctbal",
                                  "c_mktsegment", "c_comment"],
@@ -60,19 +60,6 @@ if __name__ == '__main__':
     if os.path.exists("data_station.db"):
         os.remove("data_station.db")
 
-    folders = ['SM_storage', 'Staging_storage', "integration_new/test_files/sql"]
-    for folder in folders:
-        for filename in os.listdir(folder):
-            file_path = os.path.join(folder, filename)
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-
-    data_gen(3)
-
-    exit()
-
     # Step 0: System initialization
 
     ds_config = "data_station_config.yaml"
@@ -85,16 +72,26 @@ if __name__ == '__main__':
         os.remove(log_path)
 
     # Step 1: We create two new users of the Data Station
-    num_users = 2
+    num_users = 3
+    data_gen(num_users)
+
     for i in range(num_users):
         ds.create_user(f"user{i}", "string")
 
     # Step 2: Each user uploads their share of the dataset.
-    table_names = ["customer", "lineitem", "nation", "orders", "part", "partsupp", "region", "supplier"]
-    num_tables = len(table_names)
+    partitioned_tables = ["customer", "lineitem", "orders", "part", "partsupp", "supplier"]
+    small_tables = ["nation", "region"]
+    for tbl in small_tables:
+        filename = f"integration_new/test_files/sql_plain/{tbl}0.csv"
+        f = open(filename, "rb")
+        file_bytes = f.read()
+        register_res = ds.call_api(f"user0", "register_data", None, None, f"user0",
+                                   f"{tbl}.csv", "file", f"{tbl}.csv", False, )
+        ds.call_api(f"user0", "upload_data", None, None, f"user0",
+                    register_res.de_id, file_bytes, )
     for i in range(num_users):
-        for tbl in table_names:
-            filename = f"integration_new/test_files/sql/{tbl}{i}.csv"
+        for tbl in partitioned_tables:
+            filename = f"integration_new/test_files/sql_plain/{tbl}{i}.csv"
             f = open(filename, "rb")
             file_bytes = f.read()
             register_res = ds.call_api(f"user{i}", "register_data", None, None, f"user{i}",
@@ -102,28 +99,42 @@ if __name__ == '__main__':
             ds.call_api(f"user{i}", "upload_data", None, None, f"user{i}",
                         register_res.de_id, file_bytes, )
 
-    # Step 3: user0 suggests a share saying all users can run all functions in share
-    agents = [1, 2]
+    # Step 3: user0 suggests a share saying he can run all functions in share
+    agents = [1]
     functions = ["select_star", "tpch_1", "tpch_2", "tpch_3", "tpch_4", "tpch_5", "tpch_6", "tpch_7",
                  "tpch_8", "tpch_9", "tpch_10", "tpch_11", "tpch_12", "tpch_13", "tpch_14", "tpch_15", "tpch_16",
                  "tpch_17", "tpch_18", "tpch_19", "tpch_20", "tpch_21", "tpch_22"]
-    total_des = num_users * num_tables
+    total_des = num_users * len(partitioned_tables) + len(small_tables)
     data_elements = list(range(1, total_des + 1))
     ds.call_api("user0", "suggest_share", None, None, "user0", agents, functions, data_elements)
 
     # Step 4: all users acknowledge the share
     cur_de_id = 1
     for i in range(num_users):
-        for j in range(num_tables):
+        if i == 0:
+            cur_user_de_count = len(partitioned_tables) + len(small_tables)
+        else:
+            cur_user_de_count = len(partitioned_tables)
+        for j in range(cur_user_de_count):
             ds.call_api(f"user{i}", "ack_data_in_share", None, None, f"user{i}", cur_de_id, 1)
             cur_de_id += 1
 
     # Step 5: user0 calls functions
     select_star_res = ds.call_api("user0", "select_star", 1, "pessimistic", "nation")
     print("Result of select star from nation is:", select_star_res)
-    for i in range(1, len(functions)):
-        tpch_res = ds.call_api("user0", f"tpch_{i}", 1, "pessimistic")
-        print(f"Result of TPC_H {i} is:", tpch_res)
+    # for i in range(1, len(functions)):
+    #     tpch_res = ds.call_api("user0", f"tpch_{i}", 1, "pessimistic")
+    #     print(f"Result of TPC_H {i} is:", tpch_res)
+
+    # Clean up
+    folders = ['SM_storage', 'Staging_storage', "integration_new/test_files/sql_plain"]
+    for folder in folders:
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
 
     # Last step: shut down the Data Station
     ds.shut_down()
