6d808b709537d154fbebc14b5bbcdd9019151c1f
KoSpades <steven.sy.xia@gmail.com>
2024-08-06 15:53:38 -0400

    before fixing README

--- integration_new/advertising_experiment.py
+++ integration_new/advertising_experiment.py
@@ -137,71 +137,26 @@ if __name__ == '__main__':
         f.close()
         print(ds.call_api(cur_token, "upload_data_in_csv", plaintext_bytes))
 
-    # Step 2.5: Testing short-circuit here (comment out the block below when testing intermediates)
-    # We also need the (no approved) case -> to get the results we just run the function directly.
-    # These two sets of results will both be saved.
-    DE_approval_status = "none" # partial or none
-    dest_agents = [1]
-    data_elements = [1]
-    if save_intermediate:
-        f = "train_model_over_joined_data_v1"
-    else:
-        f = "train_model_over_joined_data_v2"
-    label_name = "clicked_on_ad"
-    query = "select youtube.male, less_than_twenty_five, live_in_states, married, liked_games_page, clicked_on_ad " \
-            "from facebook inner join youtube " \
-            "on facebook.first_name = youtube.first_name " \
-            "and facebook.last_name = youtube.last_name " \
-            "and jaro_similarity(facebook.email, youtube.email) > 0.9"
-    if DE_approval_status == "partial":
-        print(ds.call_api(facebook_token, "propose_contract",
-                          dest_agents, data_elements, f, model_name, label_name, query))
-
-    if num_MB == 7:
-        num_MB = 10
-    start_time = time.time()
-    for _ in range(12):
-        run_start_time = time.time()
-        res = ds.call_api(facebook_token, f, model_name, label_name, query)
-        run_end_time = time.time()
-        with open(f"numbers/shortcircuit/{num_MB}_{DE_approval_status}_advertising.csv", "a") as file:
-            writer = csv.writer(file)
-            # Case 1: With short-circuiting: f() gets terminated
-            if res["result"] is None:
-                writer.writerow([res["experiment_time_arr"][0] - run_start_time,
-                                 res["experiment_time_arr"][1] - res["experiment_time_arr"][0],
-                                 run_end_time - res["experiment_time_arr"][1]])
-            # Case 2: No short-circuiting: f() runs to completion
-            else:
-                writer.writerow([res["experiment_time_arr"][0] - run_start_time,
-                                 res["result"][1] - res["experiment_time_arr"][0],
-                                 res["experiment_time_arr"][1] - res["result"][1],
-                                 run_end_time - res["experiment_time_arr"][1]])
-    print("Time for all runs", time.time() - start_time)
-
-    # # Step 3: Train the joint model.
+    # # Step 2.5: Testing short-circuit here (comment out the block below when testing intermediates)
+    # # We also need the (no approved) case -> to get the results we just run the function directly.
+    # # These two sets of results will both be saved.
+    # DE_approval_status = "none" # partial or none
     # dest_agents = [1]
-    # data_elements = [1, 2]
+    # data_elements = [1]
     # if save_intermediate:
     #     f = "train_model_over_joined_data_v1"
     # else:
     #     f = "train_model_over_joined_data_v2"
     # label_name = "clicked_on_ad"
-    # # "select youtube.male, less_than_twenty_five, live_in_states, married, liked_games_page, clicked_on_ad from facebook inner join youtube on facebook.first_name = youtube.first_name and facebook.last_name = youtube.last_name"
     # query = "select youtube.male, less_than_twenty_five, live_in_states, married, liked_games_page, clicked_on_ad " \
     #         "from facebook inner join youtube " \
     #         "on facebook.first_name = youtube.first_name " \
     #         "and facebook.last_name = youtube.last_name " \
     #         "and jaro_similarity(facebook.email, youtube.email) > 0.9"
-    # print(ds.call_api(facebook_token, "propose_contract",
-    #                   dest_agents, data_elements, f, model_name, label_name, query))
-    # print(ds.call_api(facebook_token, "approve_contract", 1))
-    # print(ds.call_api(youtube_token, "approve_contract", 1))
+    # if DE_approval_status == "partial":
+    #     print(ds.call_api(facebook_token, "propose_contract",
+    #                       dest_agents, data_elements, f, model_name, label_name, query))
     #
-    # # res_1 = ds.call_api(facebook_token, "train_model_over_joined_data", label_name, query)
-    # # print(res_1.coef_)
-    #
-    # # For recording time: run it 11 times (extra times for warmup)
     # if num_MB == 7:
     #     num_MB = 10
     # start_time = time.time()
@@ -209,15 +164,60 @@ if __name__ == '__main__':
     #     run_start_time = time.time()
     #     res = ds.call_api(facebook_token, f, model_name, label_name, query)
     #     run_end_time = time.time()
-    #     # 1: fixed overhead 2: join DE time 3: model train time 4: fixed overhead
-    #     with open(f"numbers/intermediate/{num_MB}_{model_name}_{save_intermediate}.csv", "a") as file:
+    #     with open(f"numbers/shortcircuit/{num_MB}_{DE_approval_status}_advertising.csv", "a") as file:
     #         writer = csv.writer(file)
-    #         if res["result"] is not None:
+    #         # Case 1: With short-circuiting: f() gets terminated
+    #         if res["result"] is None:
+    #             writer.writerow([res["experiment_time_arr"][0] - run_start_time,
+    #                              res["experiment_time_arr"][1] - res["experiment_time_arr"][0],
+    #                              run_end_time - res["experiment_time_arr"][1]])
+    #         # Case 2: No short-circuiting: f() runs to completion
+    #         else:
     #             writer.writerow([res["experiment_time_arr"][0] - run_start_time,
     #                              res["result"][1] - res["experiment_time_arr"][0],
     #                              res["experiment_time_arr"][1] - res["result"][1],
-    #                              run_end_time-res["experiment_time_arr"][1]])
+    #                              run_end_time - res["experiment_time_arr"][1]])
     # print("Time for all runs", time.time() - start_time)
+
+    # Step 3: Train the joint model.
+    dest_agents = [1]
+    data_elements = [1, 2]
+    if save_intermediate:
+        f = "train_model_over_joined_data_v1"
+    else:
+        f = "train_model_over_joined_data_v2"
+    label_name = "clicked_on_ad"
+    # "select youtube.male, less_than_twenty_five, live_in_states, married, liked_games_page, clicked_on_ad from facebook inner join youtube on facebook.first_name = youtube.first_name and facebook.last_name = youtube.last_name"
+    query = "select youtube.male, less_than_twenty_five, live_in_states, married, liked_games_page, clicked_on_ad " \
+            "from facebook inner join youtube " \
+            "on facebook.first_name = youtube.first_name " \
+            "and facebook.last_name = youtube.last_name " \
+            "and jaro_similarity(facebook.email, youtube.email) > 0.9"
+    print(ds.call_api(facebook_token, "propose_contract",
+                      dest_agents, data_elements, f, model_name, label_name, query))
+    print(ds.call_api(facebook_token, "approve_contract", 1))
+    print(ds.call_api(youtube_token, "approve_contract", 1))
+
+    # res_1 = ds.call_api(facebook_token, "train_model_over_joined_data", label_name, query)
+    # print(res_1.coef_)
+
+    # For recording time: run it 11 times (extra times for warmup)
+    if num_MB == 7:
+        num_MB = 10
+    start_time = time.time()
+    for _ in range(2):
+        run_start_time = time.time()
+        res = ds.call_api(facebook_token, f, model_name, label_name, query)
+        run_end_time = time.time()
+        # 1: fixed overhead 2: join DE time 3: model train time 4: fixed overhead
+        with open(f"numbers/intermediate/{num_MB}_{model_name}_{save_intermediate}.csv", "a") as file:
+            writer = csv.writer(file)
+            if res["result"] is not None:
+                writer.writerow([res["experiment_time_arr"][0] - run_start_time,
+                                 res["result"][1] - res["experiment_time_arr"][0],
+                                 res["experiment_time_arr"][1] - res["result"][1],
+                                 run_end_time-res["experiment_time_arr"][1]])
+    print("Time for all runs", time.time() - start_time)
 
     # Last step: shut down the Data Station
     ds.shut_down()
